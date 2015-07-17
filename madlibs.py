b38121494fde54c52fc9155f174e6b954678c3a0
Hackbright Student <info@hackbrightacademy.com>
2015-07-17 12:58:32 -0700

    changed madlib to camp

--- madlibs.py
+++ madlibs.py
@@ -38,12 +38,25 @@ def greet_person():
 
 @app.route('/madlib')
 def madlib():
-    person = request.args.get("person")
-    color = request.args.get("color")
+    relative1 = request.args.get("relative1")
+    adjective1 = request.args.get("adjective1")
+    adjective2 = request.args.get("adjective2")
+    adjective3 = request.args.get("adjective3")
+    roomPerson1 = request.args.get("roomPerson1")
+    adjective4 = request.args.get("adjective4")
+    adjective5 = request.args.get("adjective5")
+    verbEndingED = request.args.get("verbEndingED")
+    bodypart = request.args.get("bodypart")
+    verbEndingING = request.args.get("verbEndingING")
+    pluralnoun = request.args.get("pluralnoun")
     noun = request.args.get("noun")
-    adjective = request.args.get("adjective")
-    
-    return render_template("madlib-output.html", person=person, noun=noun, color=color, adjective=adjective)
+    adverb = request.args.get("adverb")
+    verb1 = request.args.get("verb1")
+    verb2 = request.args.get("verb2")
+    relative2 = request.args.get("relative2")
+    yourname = request.args.get("yourname")
+
+    return render_template("madlib-output.html", relative1=relative1, adjective1=adjective1, adjective2=adjective2, adjective3=adjective3, roomPerson1=roomPerson1, adjective4=adjective4, adjective5=adjective5, verbEndingED=verbEndingED, bodypart=bodypart, verbEndingING=verbEndingING, pluralnoun=pluralnoun, noun=noun, adverb=adverb, verb1=verb1, verb2=verb2, relative2=relative2, yourname=yourname)
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
     # if we change the code.
