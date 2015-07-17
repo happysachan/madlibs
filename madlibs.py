307d33ad7310a8609b1998d70bbbc3141623ad87
Hackbright Student <info@hackbrightacademy.com>
2015-07-16 17:18:15 -0700

    madlibs is done!

---
+++ madlibs.py
@@ -0,0 +1,51 @@
+from random import choice
+
+from flask import Flask, render_template, request
+
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+# route to handle the landing page of a website.
+@app.route('/')
+def start_here():
+    return "Hi! This is the home page."
+
+# route to display a simple web page
+@app.route('/hello') #madlib-input when game option == YES; when == no it goes goodbye; otherwise /hello
+def say_hello():
+    game_option = request.args.get("game_option")
+
+    if game_option == "YES":
+        return render_template("madlib-input.html")
+    elif game_option == "NO":
+        return render_template("goodbye.html")
+    else:
+        return render_template("hello.html")
+
+@app.route('/greet')
+def greet_person():
+    player = request.args.get("person")
+
+    AWESOMENESS = [
+        'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+        'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html", person=player, compliment=compliment)
+
+@app.route('/madlib')
+def madlib():
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+    
+    return render_template("madlib-output.html", person=person, noun=noun, color=color, adjective=adjective)
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads" our web app
+    # if we change the code.
+    app.run(debug=True)
+
